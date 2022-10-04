e3febd31b3a7bf37d9fe032b620bd70cfb1a004f
k3sh0z\roycer <roycercordova@gmail.com>
2022-10-04 01:52:00 -0500

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  HOST: 'http://localhost:8085',
+  HOST: 'http://10.100.3.23:8085',
   TOKEN_AUTH_USERNAME: '',
   TOKEN_AUTH_PASSWORD: '',
   TOKEN_NAME: '',
